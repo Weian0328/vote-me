e2e81c52a7156641e78e7c43a5208cf5074b871c
weian <kevin19960328@gmail.com>
2022-04-11 10:29:56 +0800

    CRUD

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,56 @@
+class CandidatesController < ApplicationController
+  
+    def index
+        @candidates = Candidate.all
+    end
+
+    def show
+       @candidate = Candidate.find_by(id: params[:id])
+    end
+
+
+    def new
+        @candidate = Candidate.new
+    end
+
+    def create
+
+        @candidate = Candidate.new(candidate_params)
+
+        if @candidate.save
+            flash[:notice] = "Candidate created!"
+            redirect_to '/candidates'
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @candidate = Candidate.find_by(id: params[:id]) 
+    end
+
+    def update
+        @candidate = Candidate.find_by(id: params[:id]) 
+
+        if @candidate.update(candidate_params)
+            flash[:notice] = "Candidate created!"
+            redirect_to '/candidates'
+        else
+        render :edit
+        end
+    end
+
+    def destroy
+        @candidate = Candidate.find_by(id: params[:id]) 
+        @candidate.destroy
+      
+        flash[:notice] = "Candidate delete!"
+        redirect_to '/candidates'
+    end
+
+    private
+    def candidate_params
+        params.require(:candidate).permit(:name, :party, :age, :politics)
+    end
+
+end
